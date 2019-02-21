e42555c2ac36fc1bb4301225bec80497652a24fa
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-02-21 09:07:00 +0200

    add MyTreeSet class

---
+++ src/com/divashchenko/MyTreeSet.java
@@ -0,0 +1,35 @@
+package com.divashchenko;
+
+public class MyTreeSet<T> implements MyTreeSetInterface<T> {
+    private MyTreeMap map = new MyTreeMap();
+
+    @Override
+    public void add(T key) {
+        map.put(key, null);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.size() == 0;
+    }
+
+    @Override
+    public boolean contains(T key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public boolean remove(T key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+}
