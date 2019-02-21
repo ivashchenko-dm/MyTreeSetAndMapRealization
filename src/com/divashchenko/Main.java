de275d93c57be93987a56415ae82deb79c6edd83
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-02-21 15:56:40 +0200

    add tests for TreeSet

--- src/com/divashchenko/Main.java
+++ src/com/divashchenko/Main.java
@@ -1,13 +1,14 @@
 package com.divashchenko;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.SortedMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Map<String, Integer> map = new TreeMap<>();
+        System.out.println("TreeMap");
+        System.out.println("=======================================");
+        System.out.println();
 
         MyTreeMap<Integer, String> myTree = new MyTreeMap<>();
 
@@ -42,6 +43,42 @@ public class Main {
         System.out.println("Clear!");
         myTree.clear();
         System.out.println("Size: " + myTree.size());
+        System.out.println();
 
+
+
+        // TreeSet
+        System.out.println();
+        System.out.println("TreeSet");
+        System.out.println("=======================================");
+        System.out.println();
+
+        MyTreeSet<Integer> myTreeSet = new MyTreeSet<>();
+
+        myTreeSet.add(10);
+        myTreeSet.add(20);
+        myTreeSet.add(30);
+        myTreeSet.add(40);
+        myTreeSet.add(50);
+        myTreeSet.add(60);
+
+        System.out.println("Size: " + myTreeSet.size());
+        System.out.println(myTreeSet);
+        System.out.println();
+
+        System.out.println("Contains 20? " + myTreeSet.contains(20));
+        System.out.println("Contains 70? " + myTreeSet.contains(70));
+        System.out.println();
+
+        System.out.println("Remove 30");
+        myTreeSet.remove(30);
+        System.out.println(myTreeSet);
+        System.out.println();
+
+        System.out.println("Is empty? " + myTreeSet.isEmpty());
+        System.out.println("Clear!");
+        myTreeSet.clear();
+        System.out.println("Is empty? " + myTreeSet.isEmpty());
+        System.out.println("Size: " + myTreeSet.size());
     }
 }
