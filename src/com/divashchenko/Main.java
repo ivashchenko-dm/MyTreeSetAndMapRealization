b798ce25c20002be4c92f94a25a083464f587b87
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-02-21 08:58:48 +0200

    add examples

--- src/com/divashchenko/Main.java
+++ src/com/divashchenko/Main.java
@@ -15,12 +15,28 @@ public class Main {
         myTree.put(8, "Ben");
         myTree.put(2, "Carl");
         myTree.put(12, "Nancy");
-        myTree.put(1, "Karl");
+        myTree.put(1, "Kate");
         myTree.put(14, "Mike");
 
-        System.out.println(myTree.size());
+        System.out.println("Size: " + myTree.size());
         System.out.println();
         System.out.println(myTree);
+        System.out.println();
+
+        System.out.println("Get value by key 12: " + myTree.get(12));
+        System.out.println();
+
+        System.out.println("Contains key 14? " + myTree.containsKey(14));
+        System.out.println("Contains key 144? " + myTree.containsKey(144));
+        System.out.println();
+
+        System.out.println("Contains value Ben? " + myTree.containsValue("Ben"));
+        System.out.println("Contains value Jul? " + myTree.containsValue("Jul"));
+        System.out.println();
+
+        System.out.println("Clear!");
+        myTree.clear();
+        System.out.println("Size: " + myTree.size());
 
     }
 }
